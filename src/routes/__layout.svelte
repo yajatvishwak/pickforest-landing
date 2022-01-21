f6372d1f50bd0a8d3f6792078f3abc34a01dc7c5
Yajat Vishwak <yajat472001@gmail.com>
2022-01-21 07:33:35 +0530

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import "../app.css";
+</script>
+  
+<slot />
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+    :global(body){
+        @apply bg-palette-yellow50;
+        font-family: 'Space Grotesk', sans-serif;
+    }
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        palette: {
+          yellow50: "#FFFDF0",
+          yellow100: "#FFF6D2",
+          yellow200: "#FFF8DC",
+          yellow300: "#FBE89D",
+          yellow400: "#F1C40F",
+          gray: "#E9E9E9",
+          darkGray: "#9E9E9E",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
